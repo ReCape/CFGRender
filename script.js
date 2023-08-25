654dc6dbb3d79ce639ed5e56e691814c94e0f274
Boyney <boynegregg312@gmail.com>
2023-08-25 08:48:34 -0400

    Added skin and stuff

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-async function readFile(file) {
-    result = await fetch(file);
-    return result.text();
-}
-
-var model = null;
-
-function loadCfg() {
-    readFile("model.cfg").then((text) => {
-        model=JSON.parse(text)
-        loadModels()
-    })
-}
-
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(2, 5, 10);
 var renderer = new THREE.WebGLRenderer({
-  antialias: true
+  antialias: true,
+  alpha: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const light = new THREE.AmbientLight(0xffffff, 2)
+scene.add(light)
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
@@ -28,13 +18,91 @@ camera.rotateY(0.5);
 camera.rotateZ(0.5);
 camera.position.set(27, 40, 20)
 
+async function readFile(file) {
+    result = await fetch(file);
+    return result.text();
+}
+
+var model = null;
+
+function loadCfg() {
+    readFile("model2.cfg").then((text) => {
+        model=JSON.parse(text)
+        loadModels()
+    })
+}
+
+function textureToCanvas(texture) {
+    const canvas = document.createElement( 'canvas' );
+	canvas.width = texture.image.width;
+	canvas.height = texture.image.height;
+	
+	const context = canvas.getContext( '2d', {willReadFrequently: true});
+	context.drawImage( texture.image, 0, 0 );
+
+    return [canvas, context]
+}
+function hasTransparency(context, x0, y0, w, h) {
+	const imgData = context.getImageData(x0, y0, w, h);
+	for (let x = 0; x < w; x++) {
+		for (let y = 0; y < h; y++) {
+			const offset = (x + y * w) * 4;
+			if (imgData.data[offset + 3] !== 0xff) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+function isAreaWhite(context, x0, y0, w, h) {
+	const imgData = context.getImageData(x0, y0, w, h);
+	for (let x = 0; x < w; x++) {
+		for (let y = 0; y < h; y++) {
+			const offset = (x + y * w) * 4;
+			if (!(
+				imgData.data[offset + 0] === 0xff &&
+				imgData.data[offset + 1] === 0xff &&
+				imgData.data[offset + 2] === 0xff &&
+				imgData.data[offset + 3] === 0xff
+			)) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+function isAreaBlack(context, x0, y0, w, h) {
+	const imgData = context.getImageData(x0, y0, w, h);
+	for (let x = 0; x < w; x++) {
+		for (let y = 0; y < h; y++) {
+			const offset = (x + y * w) * 4;
+			if (!(
+				imgData.data[offset + 0] === 0 &&
+				imgData.data[offset + 1] === 0 &&
+				imgData.data[offset + 2] === 0 &&
+				imgData.data[offset + 3] === 0xff
+			)) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
 const loader = new THREE.TextureLoader();
-const texture = loader.load ('texture.png', loadCfg)
+const texture = loader.load ('texture2.png', loadCfg)
 texture.wrapS = THREE.Repeat;
 texture.wrapT = THREE.Repeat;
 texture.magFilter = THREE.NearestFilter;
 texture.minFilter = THREE.LinearMipMapLinearFilter;
 texture.repeat.set( 1, 1 );
+
+const skin = loader.load ('skin.png')
+skin.wrapS = THREE.Repeat;
+skin.wrapT = THREE.Repeat;
+skin.magFilter = THREE.NearestFilter;
+skin.minFilter = THREE.LinearMipMapLinearFilter;
+skin.repeat.set( 1, 1 );
 
 var cubes = []
 var t;
@@ -44,23 +112,149 @@ var skinParts = {
     "body": [8, 12, 4, -4, 0, -2],
     "leftArm": [4, 12, 4, -8, 0, -2],
     "rightArm": [4, 12, 4, 4, 0, -2],
-    "leftLeg": [4, 12, 4, 0, -12, -2],
-    "rightLeg": [4, 12, 4, -4, -12, -2]
+    "leftLeg": [4, 12, 4, -4, -12, -2],
+    "rightLeg": [4, 12, 4, 0, -12, -2]
+}
+var slimSkinParts = {
+    "leftArm": [3, 12, 4, -7, 0, -2],
+    "rightArm": [3, 12, 4, 4, 0, -2]
 }
 
-function loadPlayer() {
-    Object.entries(skinParts).forEach((skinPart) => {
-        skinPart = skinPart[1]
-        const geometry = new THREE.BoxGeometry( skinPart[0], skinPart[1], skinPart[2] );
+var skinTextureOffsets = {
+    "head": [0, 0],
+    "body": [16, 16],
+    "leftArm": [32, 48],
+    "rightArm": [40, 16],
+    "leftLeg": [16, 48],
+    "rightLeg": [0, 16],
+    "headSecond": [32, 0],
+    "bodySecond": [16, 32],
+    "leftArmSecond": [48, 48],
+    "rightArmSecond": [40, 32],
+    "leftLegSecond": [0, 48],
+    "rightLegSecond": [0, 32]
+}
 
-        cubes.push(geometry)
+var slimAreas = [
+    [50, 16, 2, 4],
+    [54, 20, 2, 12],
+    [42, 48, 2, 4],
+    [46, 52, 2, 12],
+]
+function isSkinSlim() {
+    var canvasAndContext = textureToCanvas(skin);
+    var canvas = canvasAndContext[0];
+    var context = canvasAndContext[1];
+    var scale = canvas.width / 64.0;
+    result = false;
+    blackAreas = 0;
+    whiteAreas = 0;
+    slimAreas.forEach((slimArea) => {
+        for (let i = 0; i < slimArea.length; i++) {
+            slimArea[i] *= scale;
+        }
         
-        const material = new THREE.MeshBasicMaterial({color:0x444444, transparent: true, opacity: 0.5});
+        if (hasTransparency(context, slimArea[0], slimArea[1], slimArea[2], slimArea[3])) {
+            result = true;
+        }
+        else if (isAreaBlack(context, slimArea[0], slimArea[1], slimArea[2], slimArea[3])) {
+            blackAreas++;
+        }
+        else if (isAreaWhite(context, slimArea[0], slimArea[1], slimArea[2], slimArea[3])) {
+            whiteAreas++;
+        }
+    });
+    if (blackAreas >= 4 || whiteAreas >= 4) {
+        result = true;
+    }
+    return result;
+}
+
+const outerLayerDistance = 0.2
+
+function loadOuterLayer(skinPart) {
+    skinPartName = skinPart[0]
+        skinPart = skinPart[1]
+        const geometry = new THREE.BoxGeometry( skinPart[0]+outerLayerDistance, skinPart[1]+outerLayerDistance, skinPart[2]+outerLayerDistance );
+
+        const UVMap = generateMapping([skinPart[0],skinPart[1], skinPart[2]], skinTextureOffsets[skinPartName + "Second"], [64, 64])
+
+        var uvAttribute = geometry.attributes.uv;
+        
+        // The UV index we're editing
+        var index = 0
+
+        // For each plane
+        for ( var i = 0; i < uvAttribute.count/4; i +=1 ) {
+
+            // For each corner of plane
+            for (var j = 0; j < 4; j++) {
+                
+                var u = uvAttribute.getX( index );
+                var v = uvAttribute.getY( index );
+
+                var newU = UVMap[i][j][0]///6
+                var newV = UVMap[i][j][1]///6
+                        
+                uvAttribute.setXY( index, newU, newV );
+                index++;
+            }
+                
+        }
+        
+        const material = new THREE.MeshStandardMaterial({map:skin, transparent:true});
         
         const cube = new THREE.Mesh( geometry, material );
         cube.position.set(skinPart[3] + skinPart[0]/2, skinPart[4] + skinPart[1]/2, skinPart[5] + skinPart[2]/2)
 
         scene.add(cube)
+}
+
+function loadPlayer() {
+    var isSlim = isSkinSlim()
+    var loadingParts = skinParts;
+    if (isSlim) {
+        Object.entries(slimSkinParts).forEach((skinPart) => {
+            loadingParts[skinPart[0]] = skinPart[1];
+        })
+    }
+    Object.entries(loadingParts).forEach((skinPart) => {
+        skinPartName = skinPart[0]
+        skinPart = skinPart[1]
+        const geometry = new THREE.BoxGeometry( skinPart[0], skinPart[1], skinPart[2] );
+        const UVMap = generateMapping([skinPart[0],skinPart[1], skinPart[2]], skinTextureOffsets[skinPartName], [64, 64])
+
+        var uvAttribute = geometry.attributes.uv;
+        
+        // The UV index we're editing
+        var index = 0
+
+        // For each plane
+        for ( var i = 0; i < uvAttribute.count/4; i +=1 ) {
+
+            // For each corner of plane
+            for (var j = 0; j < 4; j++) {
+                
+                var u = uvAttribute.getX( index );
+                var v = uvAttribute.getY( index );
+
+                var newU = UVMap[i][j][0]///6
+                var newV = UVMap[i][j][1]///6
+                        
+                uvAttribute.setXY( index, newU, newV );
+                index++;
+            }
+                
+        }
+        
+        const material = new THREE.MeshStandardMaterial({map:skin});
+        
+        const cube = new THREE.Mesh( geometry, material );
+        cube.position.set(skinPart[3] + skinPart[0]/2, skinPart[4] + skinPart[1]/2, skinPart[5] + skinPart[2]/2)
+
+        scene.add(cube)
+
+        loadOuterLayer([skinPartName,skinPart])
     })
 }
 
@@ -184,7 +378,7 @@ function loadCube(pos, size, offset, textureSize, group) {
 
     cubes.push(geometry)
     
-    const material = new THREE.MeshBasicMaterial({map:texture});
+    const material = new THREE.MeshStandardMaterial({map:texture});
     
     const cube = new THREE.Mesh( geometry, material );
     cube.position.set(pos[0] + size[0]/2, pos[1] + size[1]/2, pos[2] + size[2]/2)
@@ -225,10 +419,10 @@ function loadModel(model, parent) {
 
     if (model.hasOwnProperty("rotate")) {
         rotation = model["rotate"]
-        modelGroup.rotation.x = rotation[0] * (Math.PI/180);
-        modelGroup.rotation.y = rotation[1] * (Math.PI/180);
-        modelGroup.rotation.z = rotation[2] * (Math.PI/180);
-        console.log(modelGroup.rotation.x)
+        modelGroup.rotateZ(rotation[2] * (Math.PI/180));
+        modelGroup.rotateY(rotation[1] * (Math.PI/180));
+        modelGroup.rotateX(rotation[0] * (Math.PI/180));
+        
     }
 
     if (model.hasOwnProperty("boxes")) {
